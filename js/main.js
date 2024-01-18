5e9aa67a19bbd856c5d54b9e135f383e61dbecaf
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-01-18 15:42:55 +0600

    final index.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
   $(".burger").on("click", function () {
     $(".burger").toggleClass("active");
-    $(".popup").toggleClass("active");
+    $("#header").toggleClass("active");
+  });
+
+  let prevScrollPos = window.scrollY;
+  const $header = $("#header");
+
+  $(window).on("scroll", function () {
+    const currentScrollPos = window.scrollY;
+    $header.css(
+      "top",
+      prevScrollPos > currentScrollPos || currentScrollPos === 0
+        ? "0px"
+        : "-205px"
+    );
+    $header.removeClass("active");
+    $(".burger").removeClass("active");
+    prevScrollPos = currentScrollPos;
   });
 });
